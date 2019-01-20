cae26bac86d83603f77ae3c1925922f05da40745
tguelenman <tguelenman@web.de>
2019-01-20 20:06:49 +0100

    finished basic structure

--- optimisation url generator/optimisation-url-generator/src/App.js
+++ optimisation url generator/optimisation-url-generator/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react'
 import './App.css';
+import OptUrlGen from './OptUrlGen.js';
 
 export default class App extends Component {
 	state = {
-		activeItem: "optUrlGen",
+		activeItem: 'optUrlGen',
 	}
 
-	handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+	handleItemClick = (e, { activeName }) => this.setState({ activeItem: activeName })
 
 	render() {
 		const { activeItem } = this.state
+		var optUrlGen = <OptUrlGen/>
 
 		return (
 			<div className="App">
@@ -20,21 +22,26 @@ export default class App extends Component {
 						<Menu.Item
 							name='Optimisation URL Generator'
 							active={activeItem === 'optUrlGen'}
+							activeName='optUrlGen'
 							onClick={this.handleItemClick}
 						/>
 						<Menu.Item 
 							name='Another Menu Tab 1'
 							active={activeItem === 'menu1'}
+							activeName='menu1'
 							onClick={this.handleItemClick} />
 						<Menu.Item
 							name='Another Menu Tab 2'
 							active={activeItem === 'menu2'}
+							activeName='menu2'
 							onClick={this.handleItemClick}
 						/>
 					</Menu>
 				</div>
 				<div className="Content">
-					gfdnjo
+					{this.state.activeItem === 'optUrlGen' ? optUrlGen : 
+						this.state.activeItem === 'menu1' ? '' : ''
+					}
 				</div>
 				<div className="Footer">
 					Also check out <a>https://ores.wikimedia.org/</a>

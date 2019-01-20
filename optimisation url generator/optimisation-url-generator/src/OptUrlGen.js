fe6130fd36ab2a2c4555943766d9087966031327
tguelenman <tguelenman@web.de>
2019-01-20 20:32:59 +0100

    first well working version

--- optimisation url generator/optimisation-url-generator/src/OptUrlGen.js
+++ optimisation url generator/optimisation-url-generator/src/OptUrlGen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react'
+import { Form, Input } from 'semantic-ui-react'
+import './OptUrlGen.css';
 
 const minMaxOptions = [
-  { key: 'min', text: 'Minimum', value: 'Minimum' },
-  { key: 'max', text: 'Maximum', value: 'Maximum' },
+  { key: 'min', text: 'minimum', value: 'minimum' },
+  { key: 'max', text: 'maximum', value: 'maximum' },
 ]
 
 const metricOptions = [
@@ -27,7 +28,7 @@ const glOptions = [
 export default class OptUrlGen extends Component {
 	
 	state = {
-		minMax: 'Maximum',
+		minMax: 'maximum',
 		metric1: '',
 		metric2: '',
 		lg: '>=',
@@ -57,7 +58,11 @@ export default class OptUrlGen extends Component {
 	}
 
 	//handleChange = (e, { value }) => this.setState({ value })
-	  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+	handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+
+	//TODO also for other models <- mit trennstrich zur naechsten section
+	//TODO no submit button but 1) copy to clipboard, 2) open in new tab <- only possible if all states have a value
 
 	render() {
 		const { 
@@ -71,20 +76,18 @@ export default class OptUrlGen extends Component {
 			<div className = "OptUrlGen">
 				<Form onSubmit={this.handleSubmit}>
 					<Form.Group widths='equal'>
-						<Form.Select name='minMax' fluid value={'Maximum'} options={minMaxOptions} onChange={this.handleChange}/>
+						<Form.Select name='minMax' fluid value={'maximum'} options={minMaxOptions} onChange={this.handleChange}/>
 						<Form.Select name='metric1' fluid value={metric1} placeholder='Metric 1' options={metricOptions} onChange={this.handleChange}/>
 						<p>@</p>
 						<Form.Select name='metric2' fluid value={metric2} placeholder='Metric 2' options={metricOptions}  onChange={this.handleChange}/>
 						<Form.Select name='lg' fluid value={'>='} options={glOptions}  onChange={this.handleChange}/>
 						<Form.Input name='metricFloat' fluid value={metricFloat} placeholder='value between 0.0 and 1.0'  onChange={this.handleChange}/>
 					</Form.Group>
+					<Input Id='outputUrl' name='outputUrl' value={"https://ores.wikimedia.org/v3/scores/enwiki/?models=damaging&model_info=statistics.thresholds.true.'"+this.state.minMax+" "+this.state.metric1+" @ "+this.state.metric2+" "+this.state.lg+" "+this.state.metricFloat+"'"} />
 					<Form.Button content='Submit' />
 				</Form>
-				<strong>onChange:</strong>
-				<pre>{JSON.stringify({ minMax, metric1, metric2, lg, metricFloat,}, null, 2)}</pre>
-				<strong>onSubmit:</strong>
-				<pre>{JSON.stringify({ submittedMinMax, submittedMetric1, submittedMetric2, submittedLg, submittedMetricFloat, }, null, 2)}</pre>
+				
 			</div>
-	)
-  }
+		)
+	}
 }

ac7556da17323f979d310010b120bb8cc5fe7a5c
tguelenman <tguelenman@web.de>
2019-01-28 18:21:43 +0100

    started to add more styling (results unreadable atm)

--- optimisation-url-generator/src/MetricsShow.js
+++ optimisation-url-generator/src/MetricsShow.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Input, Label} from 'semantic-ui-react';
+import { 
+	Form, Button, Input, 
+	Label } from 'semantic-ui-react';
 
 import './styling/MetricsShow.css';
 
@@ -19,19 +21,20 @@ export default class MetricsGetRequest extends Component {
 		const { metrics, } = this.state
 		console.log("MetricsShow: ",JSON.stringify(this.state.metrics))
 		
+		//TODO what to do if everything is null
 		var output = []
 		Object.keys(metrics).forEach(function(key) {
 			console.log(key,metrics[key])
 			output.push(
-				<div>
-					<Label>{key}</Label>:<Input>{metrics[key]}</Input>
-				</div>
+					<Input className='MetricLabel' label={key} value={metrics[key] ? metrics[key] : "null"} />
 			)
 		})
 		
 		return (
 			<div>
-			{output}
+				<Form id='MetricLabels'>
+					{output}
+				</Form>
 			</div>
 		)
 	}

3240d4d2dc774751bbf5a3ccffa643cea1b1df12
tguelenman <tguelenman@web.de>
2019-03-05 17:36:25 +0100

    Update Testrange.js

--- prototype/src/Testrange.js
+++ prototype/src/Testrange.js
@@ -1,72 +1,31 @@
 import React from 'react';
-import Slider from 'react-rangeslider'
-import 'react-rangeslider/lib/index.css'
+import Radar from 'react-d3-radar'
 
 import './styling/Testrange.css'
 
 
 export default class Testrange extends React.Component {
-	constructor(props, context) {
-		super(props, context)
-		this.state = {
-			threshold: 0
-		}
+
+	state = {
+
 	}
 
-	handleOnChange = (value) => {
+	componentWillReceiveProps = (nextProps) => {
 
-		this.setState({
-			threshold: value / 1000,
-			//backgroundSize: '100% '+value/10+'% !important',
-		}/*, () => {
-			
-			var upperBar = document.getElementsByClassName('rangeslider rangeslider-vertical')[0]
-			console.log("uppderBarComplete: ", document.getElementsByClassName('rangeslider rangeslider-vertical'))
-			console.log("style: ",upperBar.style)
-			console.log("styleState: ",this.state.backgroundSize)
-			upperBar.style.backgroundSize = this.state.backgroundSize
-			console.log("bgsize style: ",upperBar.style)		
-		}*/)
 	}
 	
-	componentWillReceiveProps = (nextProps) => {
-				
-		this.setState({
-			threshold: nextProps.threshold,
-		})
+	componentDidMount = () => {
+		
 	}
+	
+	
 
 	render() {
 		
-		const threshold = this.state.threshold
-		
-		const labelStyle={ marginTop: 325 - threshold * 300 }
+
 		return (
 			<div>
-				<div id='thresholdBar'>
-					<div id='thresholdTitle'>
-						<h3>Threshold: {threshold}</h3>
-					</div>
-					<div id='sliderContainer'>
-						<Slider
-							value={threshold * 1000}
-							orientation="vertical"
-							onChange={this.handleOnChange}
-							min={0}
-							max={1000}
-							steps={1}
-							tooltip={false}
-						/>
-						<div id='zeroToOneLabel'>
-							<p>1.0</p><p>0.0</p>
-						</div>
-						<div id='goodToDamagingLabel' style={labelStyle}>
-							<p>damaging</p><p>good</p>
-						</div>
-
-					</div>
-				</div>
-				<div className='triangle2'><div className='innerTriangle2'/></div>
+				nothing to test.
 			</div>
 		)
 	}

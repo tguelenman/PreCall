6b0f5a7fd72387c3c3b99fbb537d987cc91e4fb9
tguelenman <tguelenman@web.de>
2019-04-01 20:01:33 +0200

    3 bars now with circles in testrange

--- prototype/src/Testrange.js
+++ prototype/src/Testrange.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styling/Testrange.css'
 
 import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider'
+//import * as d3 from 'd3'
 
 export function Handle({ // your handle component
   handle: { id, value, percent }, 
@@ -15,18 +16,17 @@ export function Handle({ // your handle component
         marginLeft: -15,
         marginTop: 25,
         zIndex: 2,
-        width: 30,
-        height: 30,
-        border: 0,
+        width: 21,
+        height: 21,
+        border: '3px solid black',
         textAlign: 'center',
         cursor: 'pointer',
         borderRadius: '50%',
-        backgroundColor: '#2C4870',
-        color: '#333',
+        backgroundColor: 'white',
       }}
       {...getHandleProps(id)}
     >
-      <div style={{ fontFamily: 'Roboto', fontSize: 11, marginTop: -35 }}>
+      <div style={{ fontFamily: 'Arial', fontSize: 11, marginTop: -35 }}>
         {value}
       </div>
     </div>
@@ -38,10 +38,10 @@ function Track({ source, target, getTrackProps }) { // your own track component
     <div
       style={{
         position: 'absolute',
-        height: 10,
+        height: 2,
         zIndex: 1,
         marginTop: 35,
-        backgroundColor: '#546C91',
+        backgroundColor: '#989898',
         borderRadius: 5,
         cursor: 'pointer',
         left: `${source.percent}%`,
@@ -68,68 +68,81 @@ export default class Testrange extends React.Component {
 	
 	componentDidMount = () => {
 		
+		//TODO ? append circles here
+		/*
+		var radar = document.getElementById('customRadar')
+		var svg = radar.append('svg')
+			.attr('width', 500)
+			.attr('height', 500)
+			
+		console.log(svg)
+		//svg.id = 'customRadarSvg'
+		
+		//svg.id = 'customSvg'
+		//.attr('width', '500px').attr('height', '500px').append('g') //.attr('viewBox','-20 -50 450 520').append("g")
+		
+		
+		//value of width&height, num of circles, level factor
+		const wh = 500
+		const levels = 5
+		const lvlfac = (wh / levels / 2)
+		
+		
+		for(var i = 0; i < levels; i++){
+
+	
+			svg.append('svg:circle')
+				.attr('cx', wh/2)
+				.attr('cy', wh/2)
+				.attr('r', i*lvlfac)
+				.attr('class', 'radarBackgroundCircle')
+				.style('fill','transparent')
+				//.style('fill','grey').style('fill-opacity', 0.1)
+		}*/
 	}
 	
 	onUpdate1 = axis1 => {
 		if(this.state.axis1[0] !== axis1[0]){
-			this.setState({ axis1 })
+			this.setState({ axis1: axis1[0] })
 		}
 	}
 	
 	onUpdate2 = axis2 => {
 		if(this.state.axis2[0] !== axis2[0]){
-			this.setState({ axis2 })
+			this.setState({ axis2: axis2[0] })
 		}
 	}
 	
 	onUpdate3 = axis3 => {
 		if(this.state.axis3[0] !== axis3[0]){
-			this.setState({ axis3 })
+			this.setState({ axis3: axis3[0] })
 		}
 	}
 	
 	render() {
 		const { axis1, axis2, axis3, update} = this.state
-
-		const sliderStyle = {  // Give the slider some width
-			position: 'relative',
-			width: '100%',
-			height: 80,
-			border: '1px solid steelblue',
-		}
-
-		const railStyle = { 
-			position: 'absolute',
-			width: '100%',
-			height: 10,
-			marginTop: 35,
-			borderRadius: 5,
-			backgroundColor: '#8B9CB6',
-		}
 		
 		const slider1 = 
-			<Slider
-				rootStyle={sliderStyle}
-				domain={[0, 100]}
-				step={1}
+			<Slider className='slider axis1Slider'
+				domain={[0, 1]}
+				step={0.001}
 				mode={2}
 				values={[axis1]}
 				onUpdate={this.onUpdate1}
 			>
 				<Rail>
 					{({ getRailProps }) => (  // adding the rail props sets up events on the rail
-						<div style={railStyle} {...getRailProps()} /> 
+						<div className='sliderRail slider1Rail' {...getRailProps()} /> 
 					)}
 				</Rail>
 				<Handles>
 					{({ handles, getHandleProps }) => (
-						<div className="slider-handles">
+						<div className='slider-handles'>
 							{handles.map(handle => (
 								<Handle
 									key={handle.id}
 									handle={handle}
 									getHandleProps={getHandleProps}
-
 								/>
 							))}
 						</div>
@@ -152,22 +165,22 @@ export default class Testrange extends React.Component {
 			</Slider>
 			
 		const slider2 = 
-			<Slider
-				rootStyle={sliderStyle}
-				domain={[0, 100]}
-				step={1}
+			<Slider className='slider axis2Slider'
+				reversed
+				domain={[0, 1]}
+				step={0.001}
 				mode={2}
 				values={[axis2]}
 				onUpdate={this.onUpdate2}
 			>
 				<Rail>
 					{({ getRailProps }) => (  // adding the rail props sets up events on the rail
-						<div style={railStyle} {...getRailProps()} /> 
+						<div className='sliderRail slider2Rail' {...getRailProps()} /> 
 					)}
 				</Rail>
 				<Handles>
 					{({ handles, getHandleProps }) => (
-						<div className="slider-handles">
+						<div className='slider-handles'>
 							{handles.map(handle => (
 								<Handle
 									key={handle.id}
@@ -195,22 +208,23 @@ export default class Testrange extends React.Component {
 			</Slider>
 		
 		const slider3 = 
-			<Slider
-				rootStyle={sliderStyle}
-				domain={[0, 100]}
-				step={1}
+			<Slider className='slider axis3Slider'
+				vertical
+				reversed
+				domain={[0, 1]}
+				step={0.001}
 				mode={2}
 				values={[axis3]}
 				onUpdate={this.onUpdate3}
 			>
 				<Rail>
 					{({ getRailProps }) => (  // adding the rail props sets up events on the rail
-						<div style={railStyle} {...getRailProps()} /> 
+						<div className='sliderRail slider3Rail' {...getRailProps()} /> 
 					)}
 				</Rail>
 				<Handles>
 					{({ handles, getHandleProps }) => (
-						<div className="slider-handles">
+						<div className='slider-handles'>
 							{handles.map(handle => (
 								<Handle
 									key={handle.id}
@@ -239,9 +253,22 @@ export default class Testrange extends React.Component {
 		
 		return (
 			<div id='customRadar'>
-			{slider1}
-			{slider2}
-			{slider3}
+				<div id='axis1'>
+					{slider1}
+				</div>
+				<div id='axis2'>
+					{slider2}
+				</div>
+				<div id='axis3'>
+					{slider3}	
+				</div>
+				<svg width='500' height='500' id='customRadarSvg'>
+					<circle cx='250' cy='250' r='50' className='radarCircle'/>
+					<circle cx='250' cy='250' r='100' className='radarCircle'/>
+					<circle cx='250' cy='250' r='150' className='radarCircle'/>
+					<circle cx='250' cy='250' r='200' className='radarCircle'/>
+					<circle cx='250' cy='250' r='250' className='radarCircle'/>
+				</svg>
 				{JSON.stringify(this.state)}
 			</div>
 		)
